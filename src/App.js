ffc9f838fb586a50a99291024d745f1f95a754d0
xbattlax <39155588+xbattlax@users.noreply.github.com>
2022-05-03 19:06:10 +0200

    supp

--- src/App.js
+++ src/App.js
@@ -3,25 +3,13 @@ import { useState, useEffect, useMemo } from "react";
 
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import Icon from "@mui/material/Icon";
-
-import SuiBox from "components/SuiBox";
-
 //import Sidenav from "examples/Sidenav";
 //import Configurator from "examples/Configurator";
 
-import theme from "assets/theme";
 
-import createCache from "@emotion/cache";
+//import routes from "./routes";
 
-import routes from "routes";
 
-import { useSoftUIController, setMiniSidenav, setOpenConfigurator } from "context";
-
-import brand from "assets/images/logo-ct.png";
-import SignIn from "./layouts/authentication/sign-in";
 
 
 import Card from "./components/card";
@@ -29,35 +17,8 @@ import Card from "./components/card";
 export default function App() {
   const dev = true;
   const [token, setToken] = useState();
-  const [controller, dispatch] = useSoftUIController();
-  const { miniSidenav, direction, layout, openConfigurator, sidenavColor } = controller;
-  const [onMouseEnter, setOnMouseEnter] = useState(false);
   const { pathname } = useLocation();
 
-
-  // Open sidenav when mouse enter on mini sidenav
-  const handleOnMouseEnter = () => {
-    if (miniSidenav && !onMouseEnter) {
-      setMiniSidenav(dispatch, false);
-      setOnMouseEnter(true);
-    }
-  };
-
-  // Close sidenav when mouse leave mini sidenav
-  const handleOnMouseLeave = () => {
-    if (onMouseEnter) {
-      setMiniSidenav(dispatch, true);
-      setOnMouseEnter(false);
-    }
-  };
-
-  // Change the openConfigurator state
-  const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
-
-  // Setting the dir attribute for the body element
-  useEffect(() => {
-    document.body.setAttribute("dir", direction);
-  }, [direction]);
 
   // Setting page scroll to 0 when changing the route
   useEffect(() => {
@@ -65,90 +26,10 @@ export default function App() {
     document.scrollingElement.scrollTop = 0;
   }, [pathname]);
 
-  const getRoutes = (allRoutes) =>
-    allRoutes.map((route) => {
-      if (route.collapse) {
-        return getRoutes(route.collapse);
-      }
 
-      if (route.route) {
-        return <Route exact path={route.route} element={route.component} key={route.key} />;
-      }
 
-      return null;
-    });
-
-  const configsButton = (
-    <SuiBox
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      width="3.5rem"
-      height="3.5rem"
-      bgColor="white"
-      shadow="sm"
-      borderRadius="50%"
-      position="fixed"
-      right="2rem"
-      bottom="2rem"
-      zIndex={99}
-      color="dark"
-      sx={{ cursor: "pointer" }}
-      onClick={handleConfiguratorOpen}
-    >
-      <Icon fontSize="default" color="inherit">
-        settings
-      </Icon>
-    </SuiBox>
-  );
   if (dev){
-    <Card></Card>
+    return (<Card></Card>)
   }
-  if (!token){
-    return (<ThemeProvider theme={theme}>
-      <CssBaseline />
-      {layout === "dashboard" && (
-          <>
-            <Sidenav
-                color={sidenavColor}
-                brand={brand}
-                brandName="Soft UI Dashboard"
-                routes={routes}
-                onMouseEnter={handleOnMouseEnter}
-                onMouseLeave={handleOnMouseLeave}
-            />
-            <Configurator />
-            {configsButton}
-          </>
-      )}
-      <SignIn setToken={setToken}>
 
-      </SignIn>
-
-    </ThemeProvider>)
-  }
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {layout === "dashboard" && (
-        <>
-          <Sidenav
-            color={sidenavColor}
-            brand={brand}
-            brandName="Soft UI Dashboard"
-            routes={routes}
-            onMouseEnter={handleOnMouseEnter}
-            onMouseLeave={handleOnMouseLeave}
-          />
-          <Configurator />
-          {configsButton}
-        </>
-      )}
-      {layout === "vr" && <Configurator />}
-      <Routes>
-        {getRoutes(routes)}
-        <Route path="*" element={<Navigate to="/Dashboard" />} />
-      </Routes>
-    </ThemeProvider>
-  );
 }
