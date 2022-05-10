09baaefec2340b170ae9b546f44c8244a607794e
LeBonMimo <schwab.guillaume2310@gmail.com>
2022-05-10 19:50:36 +0200

    header reour en arrière

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import "../App.css"
 import "./css/header.css"
+import Card from "./Card"
+
+//images
+import arrow from "../images/arrow-left.svg"
+
+//couleurs
+const orange = "#FF8811";
+const blue = "#155A96";
+const green = "#0D7B25";
+
+
 
 const HeaderDash = (props) => {
     return(
         <div className="header">
+            <img className="retourImg" src={arrow} alt="retour en arrière"></img>
             <h1>{props.pageName}</h1>
         </div>
     );
---
+++ src/pages/RDV.js
@@ -0,0 +1,25 @@
+//components
+import Container from "../components/Container";
+import HeaderDash from "../components/HeaderDash";
+import Navbar from "../components/navbar/Navbar";
+
+
+//Images
+
+//couleurs
+const orange = "#FF8811";
+const blue = "#155A96";
+const green = "#0D7B25";
+
+
+const RDV = () => {
+    return(
+    <>
+        <HeaderDash name="Damien"></HeaderDash>
+        
+        <Navbar></Navbar>
+    </>
+    )
+    
+};
+export default RDV;
