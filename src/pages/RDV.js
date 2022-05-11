5a6f1a32e4a071fdba5097ad4556dd4481bc9a55
LeBonMimo <schwab.guillaume2310@gmail.com>
2022-05-11 09:13:13 +0200

    datepicker

--- src/pages/RDV.js
+++ src/pages/RDV.js
@@ -3,6 +3,9 @@ import Container from "../components/Container";
 import HeaderDash from "../components/HeaderDash";
 import Navbar from "../components/navbar/Navbar";
 
+//calendar
+import DatePicker from 'sassy-datepicker';
+
 
 //Images
 
@@ -13,10 +16,13 @@ const green = "#0D7B25";
 
 
 const RDV = () => {
+    const onChange = (date) => {
+        console.log(date.toString());
+    };
     return(
     <>
         <HeaderDash name="Damien"></HeaderDash>
-        
+        <DatePicker onChange={onChange} />
         <Navbar></Navbar>
     </>
     )
