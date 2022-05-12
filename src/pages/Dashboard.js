6a26b149b9f5ed04372282956efa289fc368ba25
Xbattlax <xbattlax@gmail.com>
2022-05-12 23:16:28 +0200

    api dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,7 +4,9 @@ import GrapheGlucose from "../components/GrapheGlucose";
 import HeaderDash from "../components/HeaderDash";
 import Navbar from "../components/navbar/Navbar";
 import TauxGlucose from "../components/TauxGlucose";
+import moment from "moment-timezone";
 
+import React, { useState,useEffect } from 'react';
 
 //Images
 import zigzag from '../images/zigzag.png';
@@ -16,6 +18,42 @@ const green = "#0D7B25";
 
 
 const Dashboard = () => {
+    const [glucose, setGlucose] = useState(0);
+    const [taux, setTaux] = useState([]);
+
+    useEffect(() => {
+        var date = moment().tz("Europe/Paris").format("YYYY-MM-DD HH:mm");
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                "uuid": "da61fd19-c611-43f6-8c50-4b378b9e9d3e",
+                "date": date
+            })
+        };
+        fetch('http://13.38.46.102/get_recent_glucose_record', requestOptions)
+            .then(response => response.json())
+            .then(data => setGlucose(Math.round(data.data[0].taux)));
+
+        var targetDate = moment().tz("Europe/Paris").subtract(1, "days").format("YYYY-MM-DD HH:mm");
+
+        const requestOptions2 = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                "uuid": "da61fd19-c611-43f6-8c50-4b378b9e9d3e",
+                "date": targetDate,
+                "dateNow": date
+            })
+        };
+        fetch('http://13.38.46.102/get_glucose_records', requestOptions2)
+            .then(response => response.json())
+            .then(data => setTaux(data.data.map(taux => Math.round(taux.taux))));
+
+        return () => true;
+    }, []);
+
+
     return(
     <>
         <HeaderDash name="Damien"></HeaderDash>
@@ -24,7 +62,7 @@ const Dashboard = () => {
             content="cardDash" 
             src={zigzag} 
             color={orange}
-            chiffre="96"
+            chiffre={glucose}
         >
         </TauxGlucose>
         <GrapheGlucose
@@ -41,3 +79,5 @@ const Dashboard = () => {
     
 };
 export default Dashboard;
+
+
