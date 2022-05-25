1b658d3477a2632e147627538172f5dc4345a981
Xbattlax <xbattlax@gmail.com>
2022-05-25 10:55:29 +0200

    hours sleep

--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -6,12 +6,12 @@ import Navbar from "../components/navbar/Navbar";
 import ImgConnexion from "../components/ImgConnexion";
 import Button from "../components/Button";
 import TextConnexion from "../components/TextConnexion";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useCookies } from 'react-cookie';
 import ProfilInfo from "../components/ProfilInfo";
 import ProfilCard from "../components/ProfilCard";
 import "../components/css/profil.css"
-
+import moment from "moment";
 //images
 import bedTime from "../images/bedTime.png"
 import balance from "../images/balance.png"
@@ -24,6 +24,32 @@ const lightGreen = "#9FD2AA"
 const Profil = () => {
     const [cookies, setCookie] = useCookies(['access_token', 'user']);
 
+    const [sleep, setSleep] = useState(0);
+    const [quality, setQuality] = useState(0);
+    function defstate(data) {
+        data =data.data.data[0];
+        const q = data.quality;
+        const s = data.hours_sleep;
+        setQuality(q);
+        setSleep(s);
+    }
+    useEffect(() => {
+        var date = moment().tz("Europe/Paris").format("YYYY-MM-DD HH:mm");
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                "uuid": cookies['access_token'],
+                "date": date
+            })
+        };
+
+        fetch('http://'+process.env.REACT_APP_API_URI+'/get_sleep_record', requestOptions)
+            .then(response => response.json())
+            .then(data => defstate(data))
+
+        return () => true;
+    }, []);
     return(
     <>
         <Header pageName="Profil"></Header>
@@ -34,14 +60,14 @@ const Profil = () => {
                 src={bedTime}
                 titre="Sommeil :"
                 alt="lune"
-                data = "8h"
+                data ={sleep+"h"}
             ></ProfilCard>
             <ProfilCard
                 color={orange}
                 src={balance}
                 titre="Poids :"
                 alt="Bonhomme sur une balance"
-                data = "56 Kg">
+                data = {cookies['user']['weight']+" Kg"}>
             </ProfilCard>
         </div>
         <Button
